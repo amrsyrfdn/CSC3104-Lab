63b39e1599ad0aa05ba14efbed232ac8731107f2
amirh <amirhasbull@gmail.com>
2024-12-19 20:27:36 +0800

    update

---
+++ src/main/java/com/labprojects/csc3104lab/Lab5/Q1/BMIClient.java
@@ -0,0 +1,116 @@
+package com.labprojects.csc3104lab.Lab5.Q1;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+// Main class extending Application to create a JavaFX application
+public class BMIClient extends Application {
+    // IO streams
+    DataOutputStream output = null;
+    DataInputStream input = null;
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        // Create a GridPane layout for organizing UI elements
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER); // Align the grid pane to the center
+        pane.setHgap(5.5); // Set horizontal gap between nodes
+        pane.setVgap(5.5); // Set vertical gap between nodes
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5)); // Set padding around the grid
+
+        // Add label and text field for name input
+        pane.add(new Label("Name"), 0, 0);
+        TextField namefield = new TextField();
+        pane.add(namefield, 1, 0);
+
+        // Add label and text field for weight input
+        pane.add(new Label("Weight (Kg)"), 0, 1);
+        TextField weightfield = new TextField();
+        pane.add(weightfield, 1, 1);
+
+        // Add label and text field for height input
+        pane.add(new Label("Height (m)"), 0, 2);
+        TextField heightfield = new TextField();
+        pane.add(heightfield, 1, 2);
+
+        // Create and add the "Calculate" button
+        Button button = new Button("Calculate");
+        pane.add(button, 1, 3);
+        GridPane.setHalignment(button, HPos.RIGHT); // Align the button to the right
+
+        // Create labels to display the result and additional info
+        Label result = new Label();
+        pane.add(result, 2, 0, 2, 1); // Span the result label over 2 columns
+        GridPane.setHalignment(result, HPos.LEFT); // Align result label to the left
+
+        Label bmicat = new Label(); // Label for BMI category
+        pane.add(bmicat, 2, 1, 2, 1);
+        GridPane.setHalignment(bmicat, HPos.LEFT);
+
+        Label bmiclass = new Label(); // Label for BMI classification
+        pane.add(bmiclass, 2, 2, 2, 1);
+        GridPane.setHalignment(bmiclass, HPos.LEFT);
+
+        Label risk = new Label(); // Label for health risk based on BMI
+        risk.setMinWidth(250); // Set a minimum width
+        pane.add(risk, 2, 3, 2, 1);
+        GridPane.setHalignment(risk, HPos.LEFT);
+
+
+        // Set action for the button when clicked
+        button.setOnAction(e -> {
+            try {
+                String name = namefield.getText();
+                double weight = Double.parseDouble(weightfield.getText().trim());
+                double height = Double.parseDouble(heightfield.getText().trim());
+
+                // Send the input to the server
+                output.writeUTF(name);
+                output.writeDouble(weight);
+                output.writeDouble(height);
+                output.flush();
+
+                result.setText(input.readUTF());
+                bmicat.setText(input.readUTF());
+                bmiclass.setText(input.readUTF());
+                risk.setText(input.readUTF());
+            } catch (IOException exception) {
+                System.err.println(exception);
+            }
+        });
+
+        try {
+            // Create a socket to connect to the server
+            Socket socket = new Socket("localhost", 8000);
+
+            // Create an input stream to receive data from the server
+            input = new DataInputStream(socket.getInputStream());
+
+            // Create an output stream to send data to the server
+            output = new DataOutputStream(socket.getOutputStream());
+        }
+        catch (IOException ex) {
+            result.setText(ex.toString() + '\n');
+        }
+
+
+        // Create a scene with the GridPane layout and set the stage
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("BMI Calculator"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+    }
+}
---
+++ src/main/java/com/labprojects/csc3104lab/Lab5/Q1/BMIServer.java
@@ -0,0 +1,114 @@
+package com.labprojects.csc3104lab.Lab5.Q1;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import javafx.scene.control.TextArea;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class BMIServer extends Application {
+    @Override // Override the start method in the Application class
+    public void start (Stage primaryStage){
+        // Pane to output server info
+        GridPane pane= new GridPane();
+        // Text area for displaying contents
+        TextArea ta = new TextArea();
+        ta.setPrefWidth(400); // Set preferred width
+        ta.setPrefHeight(50); // Set preferred height
+        ta.setEditable(false);
+        Label in1 = new Label();
+        Label in2 = new Label();
+        Label in3 = new Label();
+
+        pane.add(ta,0,0);
+        pane.add(in1,0,1);
+        pane.add(in2,0,2);
+        pane.add(in3,0,3);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 400, 200);
+        primaryStage.setTitle("Server"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+        new Thread( () -> {
+            try {
+                // Create a server socket
+                ServerSocket serverSocket = new ServerSocket(8000);
+                ta.appendText("Server started at " + new Date() + '\n');
+
+                while (true){
+                    // Listen for a connection request
+                    Socket socket = serverSocket.accept();
+                    // Create data input and output streams
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+                    // Read input data from the client
+                    String name = input.readUTF();
+                    double weight = input.readDouble();
+                    double height = input.readDouble();
+
+                    // Update the labels on the JavaFX Application Thread
+                    Platform.runLater(() -> {
+                        in1.setText("Input received: " + name);
+                        in2.setText("Input received: " + weight);
+                        in3.setText("Input received: " + height);
+                    });
+
+                    // Calculate BMI
+                    double bmi = weight / (height * height);
+                    String category, classification, risk;
+
+                    if (bmi < 18.5) {
+                        category = "< 18.5";
+                        classification = "Underweight";
+                        risk = "Increased";
+                    } else if (bmi >= 18.5 && bmi <= 24.9) {
+                        category = "18.5 - 24.9";
+                        classification = "Normal Weight";
+                        risk = "Least";
+                    } else if (bmi >= 25.0 && bmi <= 29.9) {
+                        category = "25.0 - 29.9";
+                        classification = "Overweight";
+                        risk = "Increased";
+                    } else if (bmi >= 30.0 && bmi <= 34.9) {
+                        category = "30.0 - 34.9";
+                        classification = "Obese class I";
+                        risk = "High";
+                    } else if (bmi >= 35.0 && bmi <= 39.9) {
+                        category = "35.0 - 39.9";
+                        classification = "Obese class II";
+                        risk = "Very High";
+                    } else {
+                        category = ">= 40.0";
+                        classification = "Obese class III";
+                        risk = "Extremely High";
+                    }
+
+                    // Send results back to the client
+                    output.writeUTF(String.format("%s's BMI is: %.2f", name, bmi));
+                    output.writeUTF(category);
+                    output.writeUTF(classification);
+                    output.writeUTF(risk);
+                    System.out.println("BMI results sent to client.");
+
+                    // Close the connection
+                    socket.close();
+                }
+            }
+            catch (IOException e){
+                    e.printStackTrace();
+            }
+        }).start();
+    }
+}
