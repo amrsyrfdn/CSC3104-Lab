c274864fc76891c35d3192a8d51cc39db1858f63
amirh <amirhasbull@gmail.com>
2024-12-28 17:48:14 +0800

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.labprojects.csc3104lab {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.desktop;
+    requires java.rmi;
 
 
     opens com.labprojects.csc3104lab to javafx.fxml;
@@ -18,9 +20,13 @@ module com.labprojects.csc3104lab {
     exports com.labprojects.csc3104lab.Lab4;
     exports com.labprojects.csc3104lab.Lab4.Q1;
     opens com.labprojects.csc3104lab.Lab4.Q1 to javafx.fxml;
-    /*opens com.labprojects.csc3104lab.Lab5 to javafx.fxml;
-    exports com.labprojects.csc3104lab.Lab5;
-    opens com.labprojects.csc3104lab.Lab6 to javafx.fxml;
+    exports com.labprojects.csc3104lab.Project;
+    opens com.labprojects.csc3104lab.Project to javafx.fxml;
+    opens com.labprojects.csc3104lab.Lab5.Q1 to javafx.fxml;
+    exports com.labprojects.csc3104lab.Lab5.Q1;
+    opens com.labprojects.csc3104lab.Lab5.Q2 to javafx.fxml;
+    exports com.labprojects.csc3104lab.Lab5.Q2;
+    /*opens com.labprojects.csc3104lab.Lab6 to javafx.fxml;
     exports com.labprojects.csc3104lab.Lab6;
     opens com.labprojects.csc3104lab.Lab7 to javafx.fxml;
     exports com.labprojects.csc3104lab.Lab7;
